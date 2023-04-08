f3b118aa91ba7f5981a0ee2c57a42a4e62f73587
Gio99c <50328709+Gio99c@users.noreply.github.com>
2023-04-07 21:39:58 -0500

    fix: tuple statistics

--- model/fad.py
+++ model/fad.py
@@ -6,7 +6,6 @@ import os
 import numpy as np
 import resampy
 import torch
-import torchmetrics
 from scipy import linalg
 from torch import nn
 from torchmetrics import Metric
@@ -142,7 +141,7 @@ class FAD(Metric):
             print("[Frechet Audio Distance] eval set dir is empty, exitting...")
             return -1
         
-        self.background_statistics = self.calculate_embd_statistics_background(target)
+        self.background_statistics = torch.tensor(np.concatenate(self.calculate_embd_statistics_background(target)))
 
     def compute(self):
         mu_eval, sigma_eval = self.calculate_embd_statistics(self.embds_lst)
