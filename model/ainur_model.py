0e7f5d5cd68f366de8ef37fbf286256d7046ef7b
Gio99c <50328709+Gio99c@users.noreply.github.com>
2023-04-10 16:03:40 -0500

    fix: test for solving parallelism issues v2

--- model/ainur_model.py
+++ model/ainur_model.py
@@ -128,8 +128,8 @@ class Ainur(L.LightningModule):
             
             with torch.no_grad():
                 # Log original audio 
-                # torchaudio.save(os.path.join(tmp_dir, "original.wav"), audio[0].detach().cpu(), 48_000) 
-                # self.logger.experiment.log_audio(os.path.join(tmp_dir, "original.wav"))
+                torchaudio.save(os.path.join(tmp_dir, "original.wav"), audio[0].detach().cpu(), 48_000) 
+                self.logger.experiment.log_audio(os.path.join(tmp_dir, "original.wav"))
 
                 # Compute fad and log audio
                 self.evaluate(text, lyrics, mode='lyrics', background=background)
@@ -318,7 +318,7 @@ if __name__ == "__main__":
                       devices=args.n_devices,
                       num_nodes=args.num_nodes,
                       default_root_dir=args.default_root_dir,
-                      num_sanity_val_steps=-1,
+                      num_sanity_val_steps=1,
                       plugins=[SLURMEnvironment(requeue_signal=signal.SIGUSR1)],
                       callbacks=[StochasticWeightAveraging(swa_lrs=1e-4), checkpoint_callback])
 
