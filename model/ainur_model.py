207fab12913f9e08041a6b8b94ece54b9654692a
Gio99c <50328709+Gio99c@users.noreply.github.com>
2023-04-14 20:10:53 +0200

    fix: gradient scheduling updated

--- model/ainur_model.py
+++ model/ainur_model.py
@@ -311,7 +311,7 @@ if __name__ == "__main__":
                   )
 
     checkpoint_callback = ModelCheckpoint(dirpath=os.path.join(args.default_root_dir, "ainur_model_v2/checkpoints/"))
-    accumulator = GradientAccumulationScheduler(scheduling={0: 8, 100: 4, 200: 2, 300:1})
+    accumulator = GradientAccumulationScheduler(scheduling={0: 4, 300: 2})
     ema = EMA(0.995)
     trainer = Trainer(max_epochs=args.epochs,
                       logger=logger,
