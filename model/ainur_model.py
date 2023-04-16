9ed6c2300f7847f64d55f26abc92a4e25d806683
Gio99c <50328709+Gio99c@users.noreply.github.com>
2023-04-16 16:47:40 +0200

    fix: ema checkpointing

--- model/ainur_model.py
+++ model/ainur_model.py
@@ -321,7 +321,7 @@ if __name__ == "__main__":
                       num_nodes=args.num_nodes,
                       default_root_dir=args.default_root_dir,
                       num_sanity_val_steps=0,
-                      gradient_clip_val=args.gradient_clip,
+                      #gradient_clip_val=args.gradient_clip,
                       plugins=[SLURMEnvironment(requeue_signal=signal.SIGUSR1)],
                       callbacks=[StochasticWeightAveraging(swa_lrs=1e-4), checkpoint_callback, accumulator, ema])
 
