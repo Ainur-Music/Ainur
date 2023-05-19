2dd4daf5303490c24a618bf2bbda82717c751c13
Gio99c <50328709+Gio99c@users.noreply.github.com>
2023-05-19 13:06:50 -0500

    fix: c3 on the same device

--- model/c3.py
+++ model/c3.py
@@ -10,12 +10,13 @@ class C3(Metric):
         self.add_state("num", torch.tensor(0), dist_reduce_fx="sum")
 
     def update(self, preds, target):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         batch_size = preds.shape[0]
         msg = f"preds shape ({preds.shape}) does not match target shape ({len(target)}) at dimension 0"
         assert preds.shape[0] == len(target), msg
 
-        preds_emb = self.clasp.encode_audio(preds)
-        target_emb = self.clasp.encode_lyrics(target)
+        preds_emb = self.clasp.encode_audio(preds).to(device)
+        target_emb = self.clasp.encode_lyrics(target).to(device)
 
         self.similarity += torch.sum(F.cosine_similarity(preds_emb, target_emb))
         self.num += batch_size
